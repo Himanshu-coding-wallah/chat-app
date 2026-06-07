120657a631ba9a478d16cf23f7ff3e76a37ba96a
Himanshu <er.himanshuk.00@gmail.com>
2026-06-07 11:58:28 +0530

    server setup

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import app from './src/app.js'
+import connectDB from './src/database/db.js'
+
+const port = process.env.PORT || 3000
+
+connectDB()
+app.listen(port, ()=>{
+    console.log(`server is running on port : ${port}`)
+})
