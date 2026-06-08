9381d4f6f5907d528eb670d347e0548c48c123a5
Himanshu <er.himanshuk.00@gmail.com>
2026-06-08 14:44:35 +0530

    basic frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Homepage from './components/Homepage.jsx'
+import Signup from './components/Signup'
+import Login from './components/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage/>
+  },
+  {
+    path: '/signup',
+    element: <Signup/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  }
+])
+
+
+function App() {
+
+  return (
+    <>
+    <div  className='p-4 h-screen flex items-center justify-center'>
+
+    <RouterProvider router={router} />
+    </div>
+    </>
+  )
+}
+
+export default App
